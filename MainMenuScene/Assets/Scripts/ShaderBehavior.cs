88616d800cbfa8a10bd021924f6b3da7db492cbf
Parag@MSI <paragmahala@gmail.com>
2019-08-25 17:27:57 +0530

    Main Menu Scene with Wobble Shaders
    I have put a button highlight and wobble shaders on buttons and the main menu background

---
+++ MainMenuScene/Assets/Scripts/ShaderBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShaderBehavior : MonoBehaviour
+{
+    Material _shaderMaterial;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _shaderMaterial = GetComponent<Image>().material;
+        Debug.Log(_shaderMaterial);
+    }
+
+
+    public void OnPointerEnter()
+    {
+        _shaderMaterial.SetFloat("_shaderRunCheck",1f);
+        Debug.Log("IN");
+    }
+
+    public void OnPointerExit()
+    {
+        _shaderMaterial.SetFloat("_shaderRunCheck", 0f);
+        Debug.Log("OUT");
+    }
+}
