32c7b52fe6935c7add62eaea33e9b1a984358c75
6ilberM <6ilberm@gmail.com>
2019-08-25 17:40:17 -0500

    Fetch Quest: added the ability to grab an object and drop it.

--- Trainee/Assets/Scripts/PlayerMovement/Grab.cs
+++ Trainee/Assets/Scripts/PlayerMovement/Grab.cs
@@ -20,42 +20,57 @@ public class Grab : MonoBehaviour
     {
 
     }
+    public string retrieveItemID()
+    {
+        if (_item != null)
+        {
+            return _item.ID;
+        }
+        else
+        {
+            return "Empty";
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && b_holding == false)
         {
             Collider2D[] hitcolliders2d = Physics2D.OverlapBoxAll(gameObject.transform.position, transform.localScale * 2, 0.0f, m_LayerMask);
 
-            int distance = 999;
+            float distance = 999;
 
             for (int i = 0; i < hitcolliders2d.Length; i++)
             {
                 //so long as obj is contains a pickup item
                 if (hitcolliders2d[i].GetComponent<a_pickupItem>())
                 {
-                    if (distance < Vector3.Magnitude(hitcolliders2d[i].gameObject.transform.position - gameObject.transform.position))
+                    if (distance > Vector3.Magnitude(hitcolliders2d[i].gameObject.transform.position - gameObject.transform.position))
                     {
+                        distance = Vector3.Magnitude(hitcolliders2d[i].gameObject.transform.position - gameObject.transform.position);
                         _item = hitcolliders2d[i].GetComponent<a_pickupItem>();
                     }
                 }
             }
-
-            if (_item != null)
+            if (Input.GetKeyDown(KeyCode.E) && _item != null)
             {
-                Debug.Log("Yo");
-
                 b_holding = true;
                 _locked = true;
             }
+
         }
 
-        // if (Input.GetKeyDown(KeyCode.E) && b_holding)
-        // {
-        //     Debug.Log("Yu");
-        //     b_holding = false;
-        // }
+        else if (b_holding == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E)&&_item != null)
+            {
+                _item= null; 
+                b_holding = false;
+                _locked = false;
+            }
+        }
+
 
         if (b_holding)
         {
