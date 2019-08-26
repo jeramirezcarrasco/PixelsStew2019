6fd6fe550babea8b3beb5dfdad0bf0d68839ed84
aleksandr bogatikov <perlexed@gmail.com>
2019-08-27 02:26:22 +0700

    Luggage Puzzle Scene

---
+++ Trainee/Assets/Scripts/LuggagePuzzle/LuggageBag.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuggageBag : MonoBehaviour
+{
+    public float InteractionTimeTreshold = 0.3f;
+    public bool CanBeRotated = true;
+
+    public bool IsInShelf { get; private set; }
+
+    public LuggageShelf Shelf;
+    public Collider2D[] BagBlocks;
+    public LuggagePuzzle LuggagePuzzle;
+
+    private bool _pickedUp = false;
+    private bool _isRotated = false;
+    private Vector2 _sourcePosition;
+    private float _lastInteractionTime = 0;
+
+    private void Start()
+    {
+        IsInShelf = false;
+        _sourcePosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(1) && _pickedUp)
+        {
+            _pickedUp = false;
+            MoveOnSourcePosition();
+        }
+
+        if (_pickedUp)
+        {
+            Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            transform.position = new Vector2(cursorPosition.x, cursorPosition.y);
+
+            if (CanBeRotated && Time.time - _lastInteractionTime > InteractionTimeTreshold)
+            {
+                if (Input.GetButton("Jump"))
+                {
+                    Rotate(_isRotated);
+                    _isRotated = !_isRotated;
+                    _lastInteractionTime = Time.time;
+                }
+            }
+        }
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (Time.time - _lastInteractionTime < InteractionTimeTreshold)
+        {
+            return;
+        }
+
+        if (!_pickedUp)
+        {
+            _pickedUp = true;
+        } else
+        {
+            if (IfCanBePlaced())
+            {
+                _pickedUp = false;
+                IsInShelf = true;
+
+                LuggagePuzzle.CheckIfBagsOnShelf();
+            } else
+            {
+                // @todo toggle red color
+            }
+        }
+
+        _lastInteractionTime = Time.time;
+    }
+
+    private void MoveOnSourcePosition()
+    {
+        transform.position = _sourcePosition;
+        if (_isRotated)
+        {
+            Rotate(true);
+        }
+    }
+
+    private bool IfCanBePlaced()
+    {
+        return Shelf.CheckIfCanBePlaced(BagBlocks);
+    }
+
+    private void Rotate(bool rotateForth)
+    {
+        transform.Rotate(0, 0, 90f * (rotateForth ? 1 : -1));
+    }
+}
---
+++ Trainee/Assets/Scripts/LuggagePuzzle/LuggagePuzzle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LuggagePuzzle : MonoBehaviour
+{
+    public LuggageBag[] bags;
+    public Text WinText;
+
+    public void CheckIfBagsOnShelf()
+    {
+        bool allBagsAreOnShelf = true;
+
+        foreach (LuggageBag bag in bags)
+        {
+            if (!bag.IsInShelf)
+            {
+                allBagsAreOnShelf = false;
+                break;
+            }
+        }
+
+        if (allBagsAreOnShelf)
+        {
+            WinText.enabled = true;
+        }
+    }
+}
