8757233955f5b0d9cf6699219695311c9e8f13bf
jorge <georgeramirez01@gmail.com>
2019-08-26 14:16:28 -0600

    more fixes

--- Trainee/Assets/Scripts/PlayerMovement/PlayerBehavior.cs
+++ Trainee/Assets/Scripts/PlayerMovement/PlayerBehavior.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 public class PlayerBehavior : MonoBehaviour
 {
     public float moveSpeed;
+    private bool FacingRight;
+    float Horizontal;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        float Horizontal = Input.GetAxis("Horizontal");
+        if (Horizontal > 0)
+        {
+            Vector3 characterScale = transform.localScale;
+            characterScale.x = -1;
+            transform.localScale = characterScale;
+
+        }
+        else if (Horizontal < 0)
+        {
+            Vector3 characterScale = transform.localScale;
+            characterScale.x = 1;
+            transform.localScale = characterScale;
+
+        }
     }
 
 
@@ -23,6 +40,11 @@ public class PlayerBehavior : MonoBehaviour
         float _hInput = Input.GetAxis("Horizontal");
         Vector3 _moveVector = new Vector3(_hInput, 0f, 0f);
         transform.Translate(_moveVector * moveSpeed * Time.deltaTime);
+    }
+
+    private void flip()
+    {
+
     }
 
     
